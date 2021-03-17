09aa440c5ea3114196e1d5185f06d8a456c07155
R. G. Esteves <rodolfo.g.esteves@intel.com>
2021-03-17 10:49:31 -0700

    Started adding cli options handling

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
-import ("bufio"
-	    "fmt"
-		"io"
-		"os"
+import (
+	"flag"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"net"
 )
 
 func main() {
-	fmt.Println(count(os.Stdin))
+	shouldCount := flag.Bool("count", false, "In counting mode (query mode if not specified)")
+	flag.Parse()
+	//dict := make(map[string]int){"one":1, "two":2,"three":3}
+	if *shouldCount == true {
+		fmt.Println("Should be counting")
+		//fmt.Println(count(os.Stdin))
+	} else {
+		conn, _ := net.Dial("tcp", "golang.org:80")
+		fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+		status, _ :=  bufio.NewReader(conn).ReadString('\n')
+		fmt.Println(status)
+	}	
+
 	fmt.Println("Done!")
 	os.Exit(0)
 }
