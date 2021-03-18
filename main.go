28a1abea34f81fb2634c2ab4722e1003b71a104f
R. G. Esteves <rodolfo.g.esteves@intel.com>
2021-03-17 23:46:20 -0700

    Trying out tags for json serialization

--- main.go
+++ main.go
@@ -1,20 +1,48 @@
 package main
 
 import (
+	"runtime"
 	"flag"
+	"log"
 	"bufio"
 	"fmt"
 	"io"
 	"os"
 	"net"
+	"encoding/json"
 )
 
+type location struct {
+	lat float64
+	long float64
+}
+
+type City struct {
+	ID string
+	Name string `json:"name"`
+	Location string `json:"location"`
+}
+
+func init() {
+	log.SetOutput(os.Stdout)
+	log.Println("Initializing in runtime ", runtime.Version())
+}
+
 func main() {
+	log.Println("Parsing command line options")
 	shouldCount := flag.Bool("count", false, "In counting mode (query mode if not specified)")
 	flag.Parse()
 	//dict := make(map[string]int){"one":1, "two":2,"three":3}
+
+	log.Println("Dispatching to actual functionality")
 	if *shouldCount == true {
 		fmt.Println("Should be counting")
+
+		myCity := City { Name: "Paris", Location: "France"}
+		fmt.Printf("The city is %v.\n", myCity)
+		bytes, err := json.Marshal(myCity)
+		exitOnError(err)
+		fmt.Println(string(bytes))
 		//fmt.Println(count(os.Stdin))
 	} else {
 		conn, _ := net.Dial("tcp", "golang.org:80")
@@ -25,6 +53,13 @@ func main() {
 
 	fmt.Println("Done!")
 	os.Exit(0)
+}
+
+func exitOnError(err error) {
+	if err != nil {
+		log.Println("Got an error: ", err)
+		os.Exit(1)
+	}
 }
 
 func count(r io.Reader) int {
